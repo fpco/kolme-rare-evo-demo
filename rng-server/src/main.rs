6c22f0e713eef124fd281e64d4702ced164a5b3d
jeffhappily <goodboy96@live.com.my>
2025-07-11 09:40:47 +0800

    Use env var for signing key

--- rng-server/src/main.rs
+++ rng-server/src/main.rs
@@ -1,10 +1,7 @@
 use axum::Json;
 use axum::{extract::State, routing::get, Router};
 use clap::{Parser, Subcommand};
-use k256::{
-    ecdsa::{signature::Signer, Signature, SigningKey},
-    elliptic_curve::rand_core::OsRng,
-};
+use k256::ecdsa::{signature::Signer, Signature, SigningKey};
 use serde::Serialize;
 use std::net::SocketAddr;
 
@@ -20,6 +17,8 @@ enum Commands {
     Serve {
         #[clap(long, env = "BIND", default_value = "[::]:3000")]
         bind: SocketAddr,
+        #[clap(long, env = "SIGNING_KEY")]
+        signing_key: String,
     },
 }
 
@@ -28,10 +27,16 @@ async fn main() -> anyhow::Result<()> {
     let cli = Cli::parse();
 
     match cli.command {
-        Commands::Serve { bind } => {
+        Commands::Serve { bind, signing_key } => {
             println!("Starting server on {}", bind);
 
-            let signing_key = SigningKey::random(&mut OsRng);
+            let key_bytes = hex::decode(signing_key)?;
+            let signing_key = SigningKey::from_bytes(
+                key_bytes
+                    .as_slice()
+                    .try_into()
+                    .map_err(|_| anyhow::anyhow!("signing_key must be 32 bytes"))?,
+            )?;
 
             let app = Router::new()
                 .route("/", get(root))
@@ -52,14 +57,18 @@ struct Response {
     signature: String,
 }
 
-async fn root(State(signing_key): State<SigningKey>) -> Json<Response> {
+async fn root(
+    State(signing_key): State<SigningKey>,
+) -> Result<Json<Response>, axum::http::StatusCode> {
     let magic_number: u32 = 42;
-    let signature: Signature = signing_key.sign(&magic_number.to_be_bytes());
+    let signature: Signature = signing_key
+        .try_sign(&magic_number.to_be_bytes())
+        .map_err(|_| axum::http::StatusCode::INTERNAL_SERVER_ERROR)?;
 
-    Json(Response {
+    Ok(Json(Response {
         number: magic_number,
         signature: signature.to_string(),
-    })
+    }))
 }
 
 async fn public_key(State(signing_key): State<SigningKey>) -> String {

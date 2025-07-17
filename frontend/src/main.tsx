a556550da401e6d78dc5ceb6c80c2ff0d76459a2
JoaquinArruiz <joaquinarruiz@gmail.com>
2025-07-17 14:24:38 -0300

    implement tanstack query

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,16 +1,21 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 
 import App from './App.tsx'
 
+const queryClient = new QueryClient()
+
 const rootElement = document.getElementById('root')
 if (rootElement) {
   createRoot(rootElement).render(
     <StrictMode>
-      <div className="min-w-screen min-h-screen flex justify-center bg-slate-950 text-white">
-        <App />
-      </div>
+      <QueryClientProvider client={queryClient}>
+        <div className="min-w-screen min-h-screen flex justify-center bg-slate-950 text-white">
+          <App />
+        </div>
+      </QueryClientProvider>
     </StrictMode>,
   )
 }

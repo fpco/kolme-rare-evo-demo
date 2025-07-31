14a571082de4be3d7289a463bed95371f954966e
JoaquinArruiz <joaquinarruiz@gmail.com>
2025-07-31 20:46:00 -0300

    remove unused hook

--- frontend/src/hooks/useUserFunds.ts
+++ frontend/src/hooks/useUserFunds.ts
@@ -1,8 +1,7 @@
-import { useQuery, useQueryClient } from '@tanstack/react-query'
+import { useQuery } from '@tanstack/react-query'
 
 import { getCurrentUser, getUserFunds, hasClaimedFunds } from '../kolmeclient'
 
-// Query key for user funds
 export const USER_FUNDS_QUERY_KEY = ['userFunds'] as const
 
 // Hook to get user funds data
@@ -25,13 +24,4 @@ export const useUserFunds = () => {
     staleTime: 0, // Always refetch when accessed
     gcTime: 0, // Don't cache in background
   })
-}
-
-// Hook to invalidate user funds query
-export const useInvalidateUserFunds = () => {
-  const queryClient = useQueryClient()
-
-  return () => {
-    queryClient.invalidateQueries({ queryKey: USER_FUNDS_QUERY_KEY })
-  }
 }

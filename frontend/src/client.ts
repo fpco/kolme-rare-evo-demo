07d9d8e68b82817bfcab44462f16c80e3a4c63db
JoaquinArruiz <joaquinarruiz@gmail.com>
2025-08-05 14:22:43 -0300

    format code

--- frontend/src/client.ts
+++ frontend/src/client.ts
@@ -1,101 +1,101 @@
-import { Buffer } from "buffer";
-import { KolmeClient } from "kolme-client";
-import { decrypt, encrypt } from "kolme-client/crypto";
+import { Buffer } from 'buffer'
+import { KolmeClient } from 'kolme-client'
+import { decrypt, encrypt } from 'kolme-client/crypto'
 
-import { API_BASE_URL } from "./api/gameApi";
+import { API_BASE_URL } from './api/gameApi'
 
-export const client = new KolmeClient(API_BASE_URL);
+export const client = new KolmeClient(API_BASE_URL)
 
 const generateRandomKey = () => {
-	return (
-		Math.random().toString(36).substring(2, 15) +
-		Math.random().toString(36).substring(2, 15)
-	);
-};
+  return (
+    Math.random().toString(36).substring(2, 15) +
+    Math.random().toString(36).substring(2, 15)
+  )
+}
 
 const getEncryptionKey = () => {
-	let encryptionKey = localStorage.getItem("encryptionKey");
-	if (!encryptionKey) {
-		encryptionKey = generateRandomKey();
-		localStorage.setItem("encryptionKey", encryptionKey);
-	}
-	return encryptionKey;
-};
+  let encryptionKey = localStorage.getItem('encryptionKey')
+  if (!encryptionKey) {
+    encryptionKey = generateRandomKey()
+    localStorage.setItem('encryptionKey', encryptionKey)
+  }
+  return encryptionKey
+}
 
-const encryptionKey = getEncryptionKey();
+const encryptionKey = getEncryptionKey()
 
 const getPrivateKey = async () => {
-	const encryptedPrivateKey = localStorage.getItem("privateKey");
+  const encryptedPrivateKey = localStorage.getItem('privateKey')
 
-	if (!encryptedPrivateKey) {
-		const privateKey = client.generatePrivateKey();
-		const encryptedPrivateKey = await encrypt({
-			message: Buffer.from(privateKey).toString("hex"),
-			key: encryptionKey,
-		});
+  if (!encryptedPrivateKey) {
+    const privateKey = client.generatePrivateKey()
+    const encryptedPrivateKey = await encrypt({
+      message: Buffer.from(privateKey).toString('hex'),
+      key: encryptionKey,
+    })
 
-		localStorage.setItem("privateKey", encryptedPrivateKey);
-		return privateKey;
-	}
+    localStorage.setItem('privateKey', encryptedPrivateKey)
+    return privateKey
+  }
 
-	const privateKey = await decrypt({
-		encryptedMessage: encryptedPrivateKey,
-		key: encryptionKey,
-	});
+  const privateKey = await decrypt({
+    encryptedMessage: encryptedPrivateKey,
+    key: encryptionKey,
+  })
 
-	return Buffer.from(privateKey, "hex");
-};
+  return Buffer.from(privateKey, 'hex')
+}
 
-export const privateKey = await getPrivateKey();
+export const privateKey = await getPrivateKey()
 
 const getPublicKey = async () => {
-	const publicKey = client.getPublicKey(privateKey);
-	return Buffer.from(publicKey).toString("hex");
-};
+  const publicKey = client.getPublicKey(privateKey)
+  return Buffer.from(publicKey).toString('hex')
+}
 
-export const publicKey = await getPublicKey();
+export const publicKey = await getPublicKey()
 
 export const hasClaimedFunds = () => {
-	return localStorage.getItem(`hasClaimedFunds_${publicKey}`) === "true";
-};
+  return localStorage.getItem(`hasClaimedFunds_${publicKey}`) === 'true'
+}
 
 export const setFundsClaimed = () => {
-	localStorage.setItem(`hasClaimedFunds_${publicKey}`, "true");
-};
+  localStorage.setItem(`hasClaimedFunds_${publicKey}`, 'true')
+}
 
 export const claimFunds = async () => {
-	if (hasClaimedFunds()) {
-		throw new Error("Funds have already been claimed for this user");
-	}
+  if (hasClaimedFunds()) {
+    throw new Error('Funds have already been claimed for this user')
+  }
 
-	const block = await client.broadcast(privateKey, [
-		{
-			App: {
-				"grab-funds": {},
-			},
-		},
-	]);
+  const block = await client.broadcast(privateKey, [
+    {
+      App: {
+        'grab-funds': {},
+      },
+    },
+  ])
 
-	setFundsClaimed();
+  setFundsClaimed()
 
-	return block;
-};
+  return block
+}
 
 export const placeBet = async (guess: number, amount: number) => {
-	if (guess < 0 || guess > 255) {
-		throw new Error("Guess must be between 0 and 255");
-	}
+  if (guess < 0 || guess > 255) {
+    throw new Error('Guess must be between 0 and 255')
+  }
 
-	const block = await client.broadcast(privateKey, [
-		{
-			App: {
-				"place-bet": {
-					guess,
-					amount: amount.toString(),
-				},
-			},
-		},
-	]);
+  const block = await client.broadcast(privateKey, [
+    {
+      App: {
+        'place-bet': {
+          guess,
+          amount: amount.toString(),
+        },
+      },
+    },
+  ])
 
-	return block;
-};
+  return block
+}
